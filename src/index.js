407837dcc083c170d9509eadcd4543c53fddfe20
Tanish Kumar Baranwal <122125236+Tanish1205@users.noreply.github.com>
2025-01-09 21:01:44 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/api', require('./Routes/CreateUser'));
-app.use('/api', require('./Routes/DisplayData'));
-app.use('/api', require('./Routes/OrderData'));
+app.use('/api', require('../Routes/CreateUser'));
+app.use('/api', require('../Routes/DisplayData'));
+app.use('/api', require('../Routes/OrderData'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
