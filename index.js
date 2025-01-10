5c6caf2301354d5a5e63841a7aa0363eea7de5f7
Tanish Kumar Baranwal <122125236+Tanish1205@users.noreply.github.com>
2025-01-10 17:22:39 +0530

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT ||  5000
 const mongoDB= require('./db')
+
+const cors = require('cors');
+
+// const app = express();
+
+// Enable All CORS Requests
+// app.use(cors());
+
+// Alternatively, enable CORS for specific origins
+const corsOptions = {
+  origin: 'https://tastyy-bites.netlify.app/', // Replace with your client's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies and authorization headers
+};
+app.use(cors(corsOptions));
+
 mongoDB(); 
 
 app.use(express.json());
